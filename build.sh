bacc601c56f8c68a7c8b6d463a7f42e85545efcf
Carl Hetherington <cth@carlh.net>
2017-07-11 14:32:44 +0100

    Various work.

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for d in ubuntu-14.04; do
+for d in debian-9-64 ubuntu-14.04-32 ubuntu-14.04-64 ubuntu-16.04-32 ubuntu-16.04-64 ubuntu-16.10-32 ubuntu-16.10-64 ubuntu-17.04-32 ubuntu-17.04-64; do
     pushd $d
     docker build -t $d .
     popd
