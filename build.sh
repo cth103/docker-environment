b9a5a84dfb38eabfc028d864016a51f3c49c39be
Carl Hetherington <cth@carlh.net>
2017-07-12 15:28:35 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-for d in debian-9-64 ubuntu-14.04-32 ubuntu-14.04-64 ubuntu-16.04-32 ubuntu-16.04-64 ubuntu-16.10-32 ubuntu-16.10-64 ubuntu-17.04-32 ubuntu-17.04-64; do
+for d in debian-7-64 debin-8-64 debian-9-64 \
+         ubuntu-14.04-32 ubuntu-14.04-64 \
+         ubuntu-16.04-32 ubuntu-16.04-64 \
+         ubuntu-16.10-32 ubuntu-16.10-64 \
+         ubuntu-17.04-32 ubuntu-17.04-64; do
     pushd $d
     docker build -t $d .
     popd
