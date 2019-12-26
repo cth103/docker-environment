8a273a1c138960764f346366088fc8b580bfc10b
Carl Hetherington <cth@carlh.net>
2019-12-27 00:37:24 +0100

    Support different user ids.

--- library.py
+++ library.py
@@ -1,4 +1,8 @@
 import os
+import pwd
+
+def insert_magic(line):
+    return line.replace('$USER_NAME', pwd.getpwuid(os.getuid()).pw_name).replace('$USER_ID', str(os.getuid()))
 
 def make_dockerfile(target):
     with open(os.path.join(target, 'Dockerfile.in'), 'r') as in_file, open(os.path.join(target, 'Dockerfile'), 'w') as out_file:
@@ -6,6 +10,6 @@ def make_dockerfile(target):
             if in_line.startswith('include'):
                 with open(in_line.split()[1], 'r') as include_file:
                     for include_line in include_file.readlines():
-                        print>>out_file,include_line.strip()
+                        print>>out_file,insert_magic(include_line.strip())
             else:
-                print>>out_file,in_line.strip()
+                print>>out_file,insert_magic(in_line.strip())
